0fff0f4295aee2f1558d1b4bfb210a1139df6374
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-10-12 15:28:39 +0200

    Movie app

--- src/pages/Tv.js
+++ src/pages/Tv.js
@@ -7,29 +7,36 @@ const Tv = () => {
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
-   
+    fetchTv();
   }, [key]);
 
   const fetchTv = () => {
     axios
       .get(
-        `https://api.themoviedb.org/3/genre/movie/list?api_key=a686a0ae574126000682bb05784b5cc6`
+        `https://api.themoviedb.org/3/discover/movie?api_key=a686a0ae574126000682bb05784b5cc6`
       )
       .then((res) => {
-        const genres = res.data.genres;
-        console.log(movieList);
-        setMovieList(genres);
+        const movies = res.data.results;
+        console.log(movies);
+        setMovieList(movies);
       });
   };
 
   return (
     <div>
-      <div>In here you look for your next TV show to watch</div>
-      <ul>
-        {movieList.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
+      <p>In here you look for your next TV show to watch</p>
+      <div className="rows" style={{ display: "flex", flexWrap: "wrap", gap: 10, justifyContent:"space-between" , alignContent:"flex-start" }}>
+        {movieList.map((movie) => (
+          <div key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="image-size"
+            />
+            <p style={{fontSize: "15px", fontWeight: "bold"}}>{movie.title}</p>
+            </div>
+         ))}
+      </div>
     </div>
   );
 };
