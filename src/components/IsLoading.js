b860f5e8d0634eefd67b5dc7660c186268808b16
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-10-14 21:15:31 +0200

    IsLoading Nasa picture

---
+++ src/components/IsLoading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const IsLoading = () => {
+  return (
+    <div  style={{
+          height: "50%",
+          fontSize: "100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+}}> IS LOADING ...</div>
+  )
+}
+
+export default IsLoading
--- src/pages/NasaPictures.js
+++ src/pages/NasaPictures.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { NasaInfo } from "../components/NasaInfo";
+import IsLoading from "../components/IsLoading";
 
 const NasaPictures = () => {
   const apis = process.env.REACT_APP_NASA_API_KEY;
 
   const [todayPicture, setTodayPicture] = useState([]);
   const [isSelected, setIseSelected] = useState("");
-    const [infoVisible, setInfoVisible] = useState(false);
-    const [isLoading, setIsLoading] = useState(false)
+  const [infoVisible, setInfoVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSelect = (todayPicture) => {
     console.log("on line 14 Nasa info the handle select is_", handleSelect);
@@ -30,6 +31,7 @@ const NasaPictures = () => {
   }, []);
 
   const getImage = () => {
+    setIsLoading(true);
     axios
       .get(
         `https://api.nasa.gov/planetary/apod?api_key=nuIBb7eoT5IwhcF4VPiVcQXh0ZB9vw7M3ZaSncqv`
@@ -38,20 +40,25 @@ const NasaPictures = () => {
         const nasaImage = res.data;
         console.log(nasaImage);
         setTodayPicture(nasaImage);
+        setIsLoading(false);
       });
   };
 
   return (
-    <div>
+    <div
+   
+    >
+          {isLoading &&
+              <IsLoading isLoading={isLoading} />}
       <div onClick={handleSelect}>
         <img
           style={{
             width: "100%",
             height: "auto",
             transition: "transform 0.2s",
-                  }}
-                  onMouseOver={((e) => e.currentTarget.style.transform = "scale(1.05)")}
-                  onMouseOut={((e) => e.currentTarget.style.transform = "scale(1)")}
+          }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.005)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
           src={todayPicture.hdurl}
           alt="{todayPicture.title}"
         />
